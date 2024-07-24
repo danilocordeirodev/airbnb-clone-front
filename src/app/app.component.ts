1e640b0973f7e16764008d017ed82e26c4dfd7a1
Danilo Cordeiro <danilocordeiro.ti@gmail.com>
2024-07-24 20:08:33 -0300

    add component to render

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,29 @@ import {
   FontAwesomeModule,
 } from '@fortawesome/angular-fontawesome';
 import { fontAwesomeIcons } from './shared/font-awesome-icons';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule, FontAwesomeModule],
+  imports: [
+    RouterOutlet,
+    ButtonModule,
+    FontAwesomeModule,
+    NavbarComponent,
+    FooterComponent,
+    ToastModule,
+  ],
+  providers: [MessageService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
   faIconLibrary = inject(FaIconLibrary);
+  isListingView = true;
 
   ngOnInit(): void {
     this.initFontAwesome();
--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
 })
-export class FooterComponent {
-
-}
+export class FooterComponent {}
